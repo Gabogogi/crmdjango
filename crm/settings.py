505ad2d00a7913f12f4b2f2e7267887b67b0a0f3
Gabogogi <117763241+Gabogogi@users.noreply.github.com>
2023-07-19 17:57:18 +0300

    Update settings.py

--- crm/settings.py
+++ crm/settings.py
@@ -133,7 +133,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static"
-django_heroku.settings(local())
+django_heroku.settings(locals())
 
 MEDIA_URL = '/images/'
 
